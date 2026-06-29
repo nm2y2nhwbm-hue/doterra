ba2eae15c5cff099a4d55ab19550b06d08076a65
nm2y2nhwbm-hue <nm2y2nhwbm@privaterelay.appleid.com>
2026-06-29 14:19:05 +0800

    Clear sensitive credentials from LineBot setup
    Reinitialize LineBot API and WebhookHandler with empty credentials to enhance security.

--- 7.0_line_bot.py
+++ 7.0_line_bot.py
@@ -3,8 +3,8 @@ from linebot.models import FlexSendMessage
 from handlers import get_drawing_response
 
 # 初始化
-line_bot_api = LineBotApi('4mTLI9JKpQXu/0Kb2qeqiOHQfjv7WFhnYpu21FG0Y8E8ob1q0YjUEc+GrtqfBZxqJQ8DoSBh+fLKPtx1zNoUHaem8j+ATxGJ9E1gpZF7UidCgSn4fAJ2WcimDRs7dZepx2m+fe1KTs6PIDZyGEtZ7AdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('eea492cdcc8c24ddc585e72367ec86fd')
+line_bot_api = LineBotApi('')
+handler = WebhookHandler('')
 
 def handle_message(event):
     if event.message.text == "抽牌":
