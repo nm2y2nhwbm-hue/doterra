951782839715475b866cbf3fc528802a4a706d9e
nm2y2nhwbm-hue <nm2y2nhwbm@privaterelay.appleid.com>
2026-06-29 15:24:15 +0800

    Refactor Line Bot API initialization and callback handling

--- 7.0_line_bot.py
+++ 7.0_line_bot.py
@@ -1,23 +1,40 @@
+import os
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from handlers import get_drawing_response
-import os
 
 app = Flask(__name__)
-line_bot_api = LineBotApi(os.environ.get('4mTLI9JKpQXu/0Kb2qeqiOHQfjv7WFhnYpu21FG0Y8E8ob1q0YjUEc+GrtqfBZxqJQ8DoSBh+fLKPtx1zNoUHaem8j+ATxGJ9E1gpZF7UidCgSn4fAJ2WcimDRs7dZepx2m+fe1KTs6PIDZyGEtZ7AdB04t89/1O/w1cDnyilFU='))
-handler = WebhookHandler(os.environ.get('eea492cdcc8c24ddc585e72367ec86fd'))
+
+# --- 修正處：直接填入字串，不要用 os.environ.get ---
+# 如果你有設定環境變數，請改為 os.environ.get('CHANNEL_ACCESS_TOKEN')
+# 但為了快速排除故障，建議先直接貼上字串測試
+LINE_CHANNEL_ACCESS_TOKEN = '4mTLI9JKpQXu/0Kb2qeqiOHQfjv7WFhnYpu21FG0Y8E8ob1q0YjUEc+GrtqfBZxqJQ8DoSBh+fLKPtx1zNoUHaem8j+ATxGJ9E1gpZF7UidCgSn4fAJ2WcimDRs7dZepx2m+fe1KTs6PIDZyGEtZ7AdB04t89/1O/w1cDnyilFU='
+LINE_CHANNEL_SECRET = 'eea492cdcc8c24ddc585e72367ec86fd'
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    handler.handle(request.get_data(as_text=True), request.headers.get('X-Line-Signature'))
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if any(k in event.message.text for k in ["抽牌", "抽卡", "draw"]):
-        reply = get_drawing_response()
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
+    user_text = event.message.text
+    if user_text == "抽牌":
+        response_text = get_drawing_response(event.source.user_id)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=response_text)
+        )
 
 if __name__ == "__main__":
-    app.run(port=int(os.environ.get('PORT', 5000)))
+    app.run()
